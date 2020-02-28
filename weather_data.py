fff0851140bd5d137046d7980f3420459971398b
Utkarsh Sharma <skhiearth@gmail.com>
2020-02-28 17:41:23 +0530

    Create weather_data.py

---
+++ weather_data.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import requests
+from datetime import datetime
+import os
+
+# Dark Sky Secret Key
+secret_key = '5fb8fb4569a3cb7c29fe665e7c8dad66'
+
+# Sonipat Coordinates
+SON_LAT = '28.9287'
+SON_LONG = '77.0912'
+
+# Reading in the AQI data
+df = pd.read_excel("AQI_Sonipat_2019/Sonipat_AQI.xlsx")
+
+# Copying data frame to new frame to extract dates
+df_date_weather = df.copy()['Date'].head(1) 
+frame = {'date': df_date_weather} 
+df_date_weather = pd.DataFrame(frame) 
+
+# Weather API
+minTemp = []
+maxTemp = []
+precipIntensity = []
+pressure = []
+humidity = []
+windSpeed = []
+windBearing = []
+cloudCover = []
+
+for date in df_date_weather['date'].dt.date:
+    date_time = str(date) + "T11:00:00"
+    link = "https://api.darksky.net/forecast/{}/{},{},{}".format(secret_key, SON_LAT, SON_LONG, date_time)
+    
+    # Sending GET request and saving the response as a response object
+    r = requests.get(url = link)
+    
+    # Unpacking data in JSON Format
+    data = r.json() 
+
+    toAdd = data['daily']['data']
+
+    for val in toAdd:
+        minTemp.append(round(((val['temperatureMin'] - 32) * 5/9), 2))
+        maxTemp.append(round(((val['temperatureMax'] - 32) * 5/9), 2))
+        precipIntensity.append(val['precipIntensity'])
+        pressure.append(val['pressure'])
+        humidity.append(val['humidity'])
+        windSpeed.append(val['windSpeed'])
+        windBearing.append(val['windBearing'])
+        cloudCover.append(val['cloudCover'])
+
+
+
+# Adding the result to the dataframe
+df_date_weather['Min_Temp'] = minTemp
+df_date_weather['Max_Temp'] = maxTemp
+df_date_weather['Precip_Intensity'] = precipIntensity
+df_date_weather['Pressure'] = pressure
+df_date_weather['Humidity'] = humidity
+df_date_weather['Wind_Speed'] = windSpeed
+df_date_weather['Wind_Bearing'] = windBearing
+df_date_weather['Cloud_Cover'] = cloudCover
+
+horizontal_stack = pd.concat([df.head(1), df_date_weather], axis=1)
+horizontal_stack = horizontal_stack.drop(['date'], axis = 1) 
+
+horizontal_stack.to_csv("AQI_Sonipat_2019/Sonipat_AQI_Weather.csv")
